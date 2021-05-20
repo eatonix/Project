d508faba525be5847eb2ba269a5b24e630d0430f
eatonix <84315455+eatonix@users.noreply.github.com>
2021-05-20 10:19:01 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,1 +1,34 @@
 
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "Header.h"
+
+void stvaranjeDatoteke(void) {
+	int podatak = 0;
+	FILE* fp = NULL;
+	fp = fopen("klijenti.bin", "rb+");
+	if (fp == NULL) {
+		printf("\nDatoteka ne postoji!\n");
+		FILE* fp1 = fopen("klijenti.bin", "wb+");
+		if (fp1 == NULL) {
+			printf("\n");
+			printf("\nDatoteku nije moguce napraviti!\n");
+			printf("\n");
+		}
+		else {
+			fwrite(&podatak, sizeof(int), 1, fp1);
+			fclose(fp1);
+			printf("\nDatoteka uspjesno kreirana.\n\n");
+		}
+	}
+	void unosKlijenta(const char* const imeDatoteke){
+		int i;
+		FILE* fp = fopen(imeDatoteke, "rb+");
+		if (fp == NULL)
+			perror("Dodavanje clana (greska)");
+			exit(EXIT_FAILURE);
+	}
+	
