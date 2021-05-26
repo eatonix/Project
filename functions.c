05f7b7512452b05a42212c4da004afded14cde75
eatonix <84315455+eatonix@users.noreply.github.com>
2021-05-26 18:45:10 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -6,73 +6,70 @@
 #include <string.h>
 #include "Header.h"
 
-void provjeraDatoteke(void) {
-	int podatak = 0;
-	FILE* fp = NULL;
-	fp = fopen("klijenti.bin", "rb+");
+void provjeraDatoteke(char* file, unsigned int* pBrojKlijenata) {
+	system ("cls");
+	
+	FILE* fp = fopen(file, "rb");
+
 	if (fp == NULL) {
-		printf("\nDatoteka ne postoji!\n");
-		FILE* fp1 = fopen("klijenti.bin", "wb+");
-		if (fp1 == NULL) {
-			printf("\n");
-			printf("\nDatoteku nije moguce napraviti!\n");
-			printf("\n");
+
+		perror("Datoteka ne postoji\n");
+
+		fp = fopen(file, "wb");
+
+		if (fp == NULL) {
+
+			perror("Datoteka se ne moze kreirati");
+			exit(EXIT_FAILURE);
 		}
 		else {
-			fwrite(&podatak, sizeof(int), 1, fp1);
-			fclose(fp1);
-			printf("\nDatoteka uspjesno kreirana.\n\n");
+
+			fwrite(pBrojKlijenata, sizeof(unsigned int), 1, fp);
+			fclose(fp);
+			printf("Datoteka kreirana\n");
 		}
 	}
-	void unosKlijenta(const char* const imeDatoteke){
-		int i;
-		FILE* fp = fopen(imeDatoteke, "rb+");
-		if (fp == NULL)
-			perror("Dodavanje clana (greska)");
-			exit(EXIT_FAILURE);
+	else {
+
+		fread(pBrojKlijenata, sizeof(unsigned int), 1, fp);
+		fclose(fp);
+
 	}
-
-void dodavanjeKlijenata(FILE* fp){
-
-    int i;
-	int* x;
-	x = (int*)calloc(1, sizeof(int));
-	*x = 0;
-	int n = 0;
-	
-	fseek(fp, 0, SEEK_SET);
-	fread(&x, sizeof(int), 1, fp);
-
-	printf("Koliko klijenata zelite dodati?");
-	scanf("%d", &n);
-
-	KLIJENT* newClients = NULL;
-	newClients = (KLIJENT*)calloc(n, sizeof(KLIJENT));
-
-	fseek(fp, 0, SEEK_END);
-	
-
-	for(i = 0; i < n; i++){
-
-		printf("%d. klijent:\n", i + 1);
-		printf("Ime: ");
-		scanf("%s", (newClients + i)->ime);
-		printf("Prezime: ");
-		scanf("%s", (newClients + i)->prezime);
-		(newClients + i)->broj_klijenata = 0;
-	}
-	*(x) += n;
-
-	fwrite(newClients, sizeof(KLIJENT), n, fp);
-	fseek(fp, 0, SEEK_SET);
-	fwrite(&x, sizeof(int), 1, fp);
-	free(x);
-	free(newClients);
-	fclose(fp);
-
-	return;
 }
-void pregledKlijenata(FILE* fp) {
+
+void dodavanjeKlijenata(char* file, unsigned int* pBrojKlijenata){
+
+   FILE* fp = NULL;
+	
+	fp = fopen(file, "rb+");
+
+	if (fp == NULL) {
+		perror("Dodavanje novog klijenta");
+		return;
+		
+	}
+	else {
+
+		KLIJENTI pKlijent = { 0 };
+		printf("Unesite ime klijenta\n");
+		scanf("%25s", pKlijent.ime);
+		printf("Unesite prezime klijenta\n");
+		scanf("%25s", pKlijent.prezime);
+		printf("Unesite broj mobitela klijenta\n");
+		scanf(" %20s", pKlijent.brojMobitela);
+		printf("Unesite email klijenta\n");
+		scanf("%25s", pKlijent.email);
+		
+		pKlijent.id = (*pBrojDevelopera)++;
+
+		fseek(fp, sizeof(unsigned int) + ((*pBrojKlijenata - 1) * sizeof(KLIJENTI)), SEEK_SET);
+		fwrite(&pKlijent, sizeof(KLIJENTI), 1, fp);
+		rewind(fp);
+		fwrite(pBrojKlijenata, sizeof(unsigned int), 1, fp);
+		fclose(fp);
+	}
+}
+void ispisKlijenata(char* file, unsigned int* pBrojKlijenata) {
 
 	int n;
 	int x = 0;
@@ -123,7 +120,7 @@ void pregledKlijenata(FILE* fp) {
 		return;
 	}
 }
-void search(FILE* fp) {
+void search(char* file, unsigned int* pBrojKlijenata) {
 
 	int num;
 	int i;
