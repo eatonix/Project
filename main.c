489aeb44679de7bd18e0af35e4d649e3cbd22b00
eatonix <84315455+eatonix@users.noreply.github.com>
2021-05-26 18:23:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,27 +6,6 @@
 int main(void) {
 	int opcija;
 	char* klijenti = "klijenti.bin";
-	unsigned int brojKlijenata = 0;
-	printf("|******************************************\
-                \n             Dobrodosli!                 \
-                \n Odaberite opciju koju zelite izvrsiti:  \
-                \n\n(1)Dodavanje novog klijenta\
-                \n(2)Pretraga klijenta\
-                \n(3)Ispis svih klijenata\
-                \n(5)Zavrsetak programa\
-                \n******************************************|\n");
-
-	do {
-		scanf("%u", &opcija);
-		if (opcija < 1 || opcija > 5) {
-			printf("Unos je neispravan, molim vas pokusajte ponovo.");
-		}
-	} while (opcija < 1 || opcija > 5);
-	switch (opcija) {
-	case '1':
-		unosKlijenta();
-		break;
-	}
-
+	fileCheck(programeri, &brojKorisnika);
 }
 
