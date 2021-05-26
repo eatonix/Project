38fae003a9254ee0357dbc3aae915df81d2ef576
eatonix <84315455+eatonix@users.noreply.github.com>
2021-05-26 18:45:06 +0200

    Update header.h

--- header.h
+++ header.h
@@ -4,14 +4,15 @@
 
 typedef struct klijenti {
 	int broj_klijenata;
-	char ime[51];
+	char ime[25];
+	char prezime[25];
 	char broj_mobitela[20];
-	char email[50];
+	char email[25];
 }KLIJENTI;
 
 void provjeraDatoteke(char*, unsigned int*);
 void dodavanjeKlijenata(char*, unsigned int*);
-void pregledKlijenata(char*, unsigned int*);
+void ispisKlijenata(char*, unsigned int*);
 void search(char*, unsigned int*);
 
 #endif HEADER_H
