e599451e12d6d10caca91c38d3cef43b769cd37a
eatonix <84315455+eatonix@users.noreply.github.com>
2021-05-26 17:45:29 +0200

    Update header.h

--- header.h
+++ header.h
@@ -2,11 +2,12 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-typedef struct kontakt {
+typedef struct klijenti {
 	int broj_klijenata;
 	char ime[51];
 	char broj_mobitela[20];
 	char email[50];
-}KONTAKT;
+}KLIJENTI;
+
 
 #endif
