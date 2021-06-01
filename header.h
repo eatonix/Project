2d46f8ec0e4eab760358f19609fbf705170b9df4
eatonix <84315455+eatonix@users.noreply.github.com>
2021-06-01 10:17:57 +0200

    Update header.h

--- header.h
+++ header.h
@@ -2,6 +2,7 @@
 #define HEADER_H
 
 typedef struct klijenti {
+	int id;
 	int broj_klijenata;
 	char ime[25];
 	char prezime[25];
@@ -13,5 +14,7 @@ void dodavanjeKlijenata(char*, unsigned int*);
 void ispisKlijenata(char*, unsigned int*);
 void izlaz(void);
 void brisanjeDatoteke(char* file);
+void pretragaPoImenu(char* file, unsigned int* pBrojKlijenata);
 
 #endif HEADER_H
+
